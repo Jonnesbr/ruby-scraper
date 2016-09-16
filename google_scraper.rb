4aff715766c58e35385470312d74640e9e744e88
João Paulo de Oliveira <jpaulo.bcc@gmail.com>
2016-09-16 14:26:45 -0300

    first commit

---
+++ google_scraper.rb
@@ -0,0 +1,30 @@
+require 'mechanize'
+module Scrapers
+	class GoogleScraper
+		BASE_URL = 'https://www.google.com.br/search?'
+		def initialize
+			@agent = Mechanize.new
+		end
+		def run(search='')
+				@search = search
+				arg = {q: @search}
+				@agent.get(BASE_URL, arg)
+				doc = @agent.page
+				doc_parse(doc)
+		end
+		def doc_parse(doc)
+			results_array = []
+			doc.parser.css('.g').each do |parse_class|
+					result_hash = {
+						name: 		 parse_class.css('.r a').map(&:text)[0],
+						dns: 		 parse_class.css('.s cite').map(&:text).map(&:strip)[0],
+						description: parse_class.css('.s span').map(&:text).last
+					}
+					results_array << result_hash
+			end
+			p results_array
+		end
+	end
+end
+
+Scrapers::GoogleScraper.new.run('univem')
